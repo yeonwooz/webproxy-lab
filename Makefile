067890ea06724331ba5c9b0402a8ea3930a0b0ed
suyeon woo <woosean999@gmail.com>
2022-11-09 10:37:01 +0000

    hash.c 파일 생성

--- Makefile
+++ Makefile
@@ -8,7 +8,7 @@ CC = gcc
 CFLAGS = -g -Wall
 LDFLAGS = -lpthread
 
-all: proxy client
+all: proxy client hash
 
 csapp.o: csapp.c csapp.h
 	$(CC) $(CFLAGS) -c csapp.c
@@ -18,6 +18,12 @@ client.o: client.c csapp.h
 
 client: client.o csapp.o
 	$(CC) $(CFLAGS) client.o csapp.o -o client 
+
+hash.o: hash.c csapp.h
+	$(CC) $(CFLAGS) -c hash.c
+
+hash: hash.o csapp.o
+	$(CC) $(CFLAGS) -c hash.o csapp.o -o hash
 
 proxy.o: proxy.c csapp.h
 	$(CC) $(CFLAGS) -c proxy.c
--- proxy.c
+++ proxy.c
@@ -1,5 +1,5 @@
 #include "csapp.h"
-#include "echo.c"
+#include "hash.c"
 
 /* Recommended max cache and object sizes */
 #define MAX_CACHE_SIZE 1049000
@@ -15,66 +15,6 @@
 //   char storage[MAX_CACHE_SIZE];
 // };
 
-int found = 0;
-
-/* 해시테이블  https://stackoverflow.com/questions/21850356/error-conflicting-types-for-strdup */
-struct nlist { /* table entry: */
-    struct nlist *next; /* next entry in chain */
-    char *name; /* defined name */
-    char *defn; /* replacement text */
-};
-
-#define HASHSIZE MAX_CACHE_SIZE
-static struct nlist *hashtab[HASHSIZE]; /* pointer table */
-
-/* hash: form hash value for string s */
-unsigned hash(char *s)
-{
-    unsigned hashval;
-    for (hashval = 0; *s != '\0'; s++)
-      hashval = *s + 31 * hashval;
-    return hashval % HASHSIZE;
-}
-
-/* lookup: look for s in hashtab */
-struct nlist *lookup(char *s)
-{
-    printf("looking for %s\n", s);
-    struct nlist *np;
-    for (np = hashtab[hash(s)]; np != NULL; np = np->next)
-        if (strcmp(s, np->name) == 0)
-          return np; /* found */
-    return NULL; /* not found */
-}
-
-char *hashstrdup(char *s);  // strdup 라는 이름은 사용할 수 없음(string.h에 이미 정의됨)
-/* install: put (name, defn) in hashtab */
-struct nlist *install(char *name, char *defn)
-{
-    struct nlist *np;
-    unsigned hashval;
-    if ((np = lookup(name)) == NULL) { /* not found */
-        np = (struct nlist *) malloc(sizeof(*np));
-        if (np == NULL || (np->name = hashstrdup(name)) == NULL)
-          return NULL;
-        hashval = hash(name);
-        np->next = hashtab[hashval];
-        hashtab[hashval] = np;
-    } else /* already there */
-        free((void *) np->defn); /*free previous defn */
-    if ((np->defn = hashstrdup(defn)) == NULL)
-       return NULL;
-    return np;
-}
-
-char *hashstrdup(char *s) /* make a duplicate of s */
-{
-    char *p;
-    p = (char *) malloc(strlen(s)+1); /* +1 for ’\0’ */
-    if (p != NULL)
-       strcpy(p, s);
-    return p;
-}
 
 void doit(int connfd);
 void clienterror(int fd, char *cause, char *errnum, 
